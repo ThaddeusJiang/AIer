52b9a33af52914579a48f8628eaf03a9f974b292
TJ <thaddeusjiang@gmail.com>
2023-03-21 11:06:31 +0900

    fix: 🐛 stripe buffer
    https://github.com/stripe/stripe-node/blob/master/examples/webhook-signing/nextjs/pages/api/webhooks.ts#L58

--- pages/api/webhooks.ts
+++ pages/api/webhooks.ts
@@ -16,13 +16,22 @@ export const config = {
   }
 };
 
-async function buffer(readable: Readable) {
-  const chunks = [];
-  for await (const chunk of readable) {
-    chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk);
-  }
-  return Buffer.concat(chunks);
-}
+// MEMO: https://github.com/stripe/stripe-node/blob/master/examples/webhook-signing/nextjs/pages/api/webhooks.ts#L58
+const buffer = (req: NextApiRequest) => {
+  return new Promise<Buffer>((resolve, reject) => {
+    const chunks: Buffer[] = [];
+
+    req.on('data', (chunk: Buffer) => {
+      chunks.push(chunk);
+    });
+
+    req.on('end', () => {
+      resolve(Buffer.concat(chunks));
+    });
+
+    req.on('error', reject);
+  });
+};
 
 const relevantEvents = new Set([
   'product.created',

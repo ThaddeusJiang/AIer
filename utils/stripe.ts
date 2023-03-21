8a5af4ad681618b7dd5bff9d23f78cfe8010d7e6
TJ <thaddeusjiang@gmail.com>
2023-03-21 11:53:04 +0900

    fix: @ts-ignore stripe apiVersion

--- utils/stripe.ts
+++ utils/stripe.ts
@@ -4,7 +4,8 @@ export const stripe = new Stripe(
   process.env.STRIPE_SECRET_KEY_LIVE ?? process.env.STRIPE_SECRET_KEY ?? '',
   {
     // https://github.com/stripe/stripe-node#configuration
-    apiVersion: '2022-11-15',
+    // @ts-ignore
+    apiVersion: '2020-08-27',
     // Register this as an official Stripe plugin.
     // https://stripe.com/docs/building-plugins#setappinfo
     appInfo: {

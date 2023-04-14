2c48f0fbd53e26f8552e6981164db9fdeec85d80
TJ <thaddeusjiang@gmail.com>
2023-04-14 13:56:40 +0900

    feat: 🎸 Collect. Train. Interact. For PKM

--- components/lp/SecondaryFeatures.jsx
+++ components/lp/SecondaryFeatures.jsx
@@ -1,8 +1,7 @@
-import { useId } from "react";
-
 import Image from "next/image";
 
 import { Tab } from "@headlessui/react";
+import { IconDatabase, IconMessage, IconTransform } from "@tabler/icons-react";
 
 import clsx from "clsx";
 
@@ -13,84 +12,39 @@ import screenshotMemo from "~/images/screenshots/memo.png";
 
 const features = [
   {
-    name: "Data",
+    name: "Collect",
     summary: "Rapidly import and automatically integrate data",
     description:
       "Quickly import data through various methods such as bulk import, daily writing, and API integration, allowing your AI avatar to have access to more information and knowledge.",
     image: screenshotMemo,
-    icon: function ReportingIcon() {
-      let id = useId();
-      return (
-        <>
-          <defs>
-            <linearGradient id={id} x1="11.5" y1={18} x2={36} y2="15.5" gradientUnits="userSpaceOnUse">
-              <stop offset=".194" stopColor="#fff" />
-              <stop offset={1} stopColor="#6692F1" />
-            </linearGradient>
-          </defs>
-          <path
-            d="m30 15-4 5-4-11-4 18-4-11-4 7-4-5"
-            stroke={`url(#${id})`}
-            strokeWidth={2}
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </>
-      );
-    }
+    icon: <IconDatabase size={80} stroke={1} color="#fff" />
   },
   {
-    name: "Training",
+    name: "Train",
     summary: "Specifically trained with personalized",
     description:
       "Our AI Avatars are not just any generic bots - they are specifically trained with your data to create a personalized and authentic experience. ",
     image: LogoOpenAI,
-    icon: function InventoryIcon() {
-      return (
-        <>
-          <path opacity=".5" d="M8 17a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H9a1 1 0 0 1-1-1v-2Z" fill="#fff" />
-          <path opacity=".3" d="M8 24a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H9a1 1 0 0 1-1-1v-2Z" fill="#fff" />
-          <path d="M8 10a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H9a1 1 0 0 1-1-1v-2Z" fill="#fff" />
-        </>
-      );
-    }
+    icon: <IconTransform size={80} stroke={1} color="#fff" />
   },
   {
-    name: "Interacting",
+    name: "Interact",
     summary: "Human-like Interactions",
     description:
       "Our AI avatars interact with you like a real person, with emotions, style, and humor. Get ready for personalized, human-like interactions that will make you forget you're talking to a machine.",
     image: screenshotChat,
-    icon: function ContactsIcon() {
-      return (
-        <>
-          <path
-            opacity=".5"
-            d="M25.778 25.778c.39.39 1.027.393 1.384-.028A11.952 11.952 0 0 0 30 18c0-6.627-5.373-12-12-12S6 11.373 6 18c0 2.954 1.067 5.659 2.838 7.75.357.421.993.419 1.384.028.39-.39.386-1.02.036-1.448A9.959 9.959 0 0 1 8 18c0-5.523 4.477-10 10-10s10 4.477 10 10a9.959 9.959 0 0 1-2.258 6.33c-.35.427-.354 1.058.036 1.448Z"
-            fill="#fff"
-          />
-          <path
-            d="M12 28.395V28a6 6 0 0 1 12 0v.395A11.945 11.945 0 0 1 18 30c-2.186 0-4.235-.584-6-1.605ZM21 16.5c0-1.933-.5-3.5-3-3.5s-3 1.567-3 3.5 1.343 3.5 3 3.5 3-1.567 3-3.5Z"
-            fill="#fff"
-          />
-        </>
-      );
-    }
+    icon: <IconMessage size={80} stroke={1} color="#fff" />
   }
 ];
 
 function Feature({ feature, isActive, className, ...props }) {
   return (
     <div className={clsx(className, !isActive && "opacity-75 hover:opacity-100")} {...props}>
-      <div className={clsx("w-9 rounded-lg", isActive ? "bg-blue-600" : "bg-slate-500")}>
-        <svg aria-hidden="true" className="h-9 w-9" fill="none">
-          <feature.icon />
-        </svg>
-      </div>
-      <h3 className={clsx("mt-6 text-sm font-medium", isActive ? "text-blue-600" : "text-slate-600")}>
+      <div className={clsx("h-20 w-20 rounded-lg", isActive ? "bg-blue-600" : "bg-slate-500")}>{feature.icon}</div>
+      <h3 className={clsx("mt-6 text-3xl font-medium", isActive ? "text-blue-600" : "text-slate-600")}>
         {feature.name}
       </h3>
-      <p className="mt-2 font-display text-xl text-slate-900">{feature.summary}</p>
+      <p className="mt-2 font-display text-lg text-slate-900">{feature.summary}</p>
       <p className="mt-4 text-sm text-slate-600">{feature.description}</p>
     </div>
   );
@@ -177,8 +131,7 @@ export function SecondaryFeatures() {
             Train AI Avatars with your data
           </h2>
           <p className="mt-4 text-lg tracking-tight text-slate-700">
-            Automatically integrate your notes, social media, and other data and self-train. You'll have more time to
-            focus on what really matters to you.
+            Collect, Train, Interact. For the most personalized AI digital avatars.
           </p>
         </div>
         <FeaturesMobile />
